94c88e4f18c6f858e3c3047b5622b66a26e0d612
bhavani902 <govrayapallybhavani@gmail.com>
2022-06-19 22:18:03 +0530

    my commit

--- ECP_app/urls.py
+++ ECP_app/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.urls import re_path as url
 from . import views
 urlpatterns = [
 	path('',views.homepage,name='home'),
