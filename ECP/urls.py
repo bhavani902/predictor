bc5093729f7af897f4b877c6caa3333759ebcb28
bhavani902 <govrayapallybhavani@gmail.com>
2022-06-19 22:29:24 +0530

    commit

--- ECP/urls.py
+++ ECP/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,re_path
-from django.urls import re_path as url
+from django.urls import include,re_path
+from django.urls import re_path 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
@@ -23,8 +23,8 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('',include('ECP_app.urls')),
+  re_path('admin/', admin.site.urls),
+  re_path('',include('ECP_app.urls')),
 
   url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 
--- ECP_app/urls.py
+++ ECP_app/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path,include,re_path
-from django.urls import re_path as url
+from django.urls import include
+from django.urls import re_path 
 from . import views
 urlpatterns = [
-	path('',views.homepage,name='home'),
-	path('colleges/',views.college_list,name='colleges'),
-	path('college/<str:pk>/',views.college_branch,name='college_branch'),
-	path('college/<str:pk1>/<str:pk2>',views.college_branch_student,name='cbs'),
+	re_path('',views.homepage,name='home'),
+	re_path('colleges/',views.college_list,name='colleges'),
+	re_path('college/<str:pk>/',views.college_branch,name='college_branch'),
+	re_path('college/<str:pk1>/<str:pk2>',views.college_branch_student,name='cbs'),
 ]
